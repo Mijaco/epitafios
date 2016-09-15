c90daf2cec2f00bf5b568ce2a0074aa97784a213
MijailAymara <casio_soe@hotmail.com>
2016-09-15 01:50:39 -0500

    COMMIT - PAGE PRINCIPAL

--- epitafio-rest/src/main/webapp/oasis/js/controller.js
+++ epitafio-rest/src/main/webapp/oasis/js/controller.js
@@ -55,6 +55,35 @@ function configuracionController($scope, $rootScope, $cookieStore, $location,fil
         alert('Removiendo Token queda > ' + $cookieStore.get('authToken'));
         $location.path("/login");
     };
+    
+    
+     var tabClasses;
+  
+  function initTabs() {
+    tabClasses = ["","","",""];
+  }
+  
+  $scope.getTabClass = function (tabNum) {
+    return tabClasses[tabNum];
+  };
+  
+  $scope.getTabPaneClass = function (tabNum) {
+    return "tab-pane " + tabClasses[tabNum];
+  }
+  
+  $scope.setActiveTab = function (tabNum) {
+    initTabs();
+    tabClasses[tabNum] = "active";
+  };
+  
+  $scope.tab1 = "This is first section";
+  $scope.tab2 = "This is SECOND section";
+  $scope.tab3 = "This is THIRD section";
+  $scope.tab4 = "This is FOUTRH section";
+  
+  //Initialize 
+  initTabs();
+  $scope.setActiveTab(1);
 
 }
 ;
