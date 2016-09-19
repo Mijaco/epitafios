49903d000ab7bbcce75ac9e05b3b1b5f21e9bc26
UserTBS1 <UserTBS1@TBS-CDB-052>
2016-09-19 18:48:15 -0500

    commit epitafio rest

--- epitafio-rest/src/main/java/com/epitafio/web/controller/PersonalizacionController.java
+++ epitafio-rest/src/main/java/com/epitafio/web/controller/PersonalizacionController.java
@@ -47,6 +47,8 @@ public class PersonalizacionController {
 
         System.out.println("distrito >>>> " + distrito);
         System.out.println("idbussines >>>> " + idbussines);
+        
+        
         HTMLMain main = userService.obtenerHtmlPersonalizado(distrito, idbussines);
 
         try {
