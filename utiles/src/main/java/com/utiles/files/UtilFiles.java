93307324e82149bca5daaf6968be34b14b7a3104
UserTBS1 <UserTBS1@TBS-CDB-052>
2016-09-19 19:10:34 -0500

    commit epitafio beans

--- utiles/src/main/java/com/utiles/files/UtilFiles.java
+++ utiles/src/main/java/com/utiles/files/UtilFiles.java
@@ -3,41 +3,64 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.utiles.files;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  *
  * @author Mijail Aymara
  */
 public class UtilFiles {
-    
-        public static void generarArchivo(String rutaNombreArchivo, String contenidoCadena){
-         
+
+    public static void generarArchivo(String rutaNombreArchivo, String contenidoCadena) {
+
         File archivoCrear;
         BufferedWriter output = null;
-        
+
         try {
             archivoCrear = new File(rutaNombreArchivo);
             output = new BufferedWriter(new FileWriter(archivoCrear));
             output.write(contenidoCadena);
-            
-        } catch ( IOException e ) {
+
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if ( output != null ) {
+            if (output != null) {
                 try {
                     output.close();
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
             }
-                archivoCrear = null;
+            archivoCrear = null;
         }
-     }
+    }
+
+    public static void salvarInputStreamEnArchivo(InputStream uploadedInputStream,
+            String uploadedFileLocation) {
+
+        try {
+            OutputStream out = null;
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            out = new FileOutputStream(new File(uploadedFileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
 }
