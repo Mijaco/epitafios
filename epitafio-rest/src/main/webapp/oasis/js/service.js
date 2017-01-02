0097f31379d66d35938356db91bb9662b3ff26f9
MijailAymara <casio_soe@hotmail.com>
2017-01-01 22:33:11 -0500

    COMMIT - PAGE PRINCIPAL

--- epitafio-rest/src/main/webapp/oasis/js/service.js
+++ epitafio-rest/src/main/webapp/oasis/js/service.js
@@ -37,7 +37,7 @@ services.service('fileUpload', ['$http', function($http) {
         this.uploadFileToUrl = function(file, uploadUrl) {
             var fd = new FormData();
             fd.append('file', file);
-            $http.post("/oasis/rest/user/salvarLogoGenerado", fd, {transformRequest: angular.identity, headers: {'Content-Type': undefined}}).success(function() {
+            $http.post("/oasis/rest/user/salvarFile", fd, {transformRequest: angular.identity, headers: {'Content-Type': undefined}}).success(function() {
             }).error(function() {
             });
         }
